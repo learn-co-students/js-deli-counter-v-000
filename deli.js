a3eeb67e1e342c8f0ed92711c06dec98112d8c00
Adam Dorsky <ebshimon@gmail.com>
2016-05-26 00:01:42 -0400

    Done.

--- deli.js
+++ deli.js
@@ -7,21 +7,26 @@ function takeANumber(line, person) {
 }
 
 function nowServing(line) {
-    if(line.size == 0) {
+    if(line.length == 0) {
         return "There is nobody waiting to be served!"
     } else {
-        var nextUp = line.shift;
-        return "Currently serving " + nextUp;
+        var nextUp = line[0];
+        line.splice(0, 1);
+        return "Currently serving " + nextUp + ".";
     }
 }
 
 function currentLine(line) {
-    if(line.size == 0) {
+    if(line.length == 0) {
         return "The line is currently empty."
     } else {
         var lineReport = "The line is currently:"
         line.forEach(function(person, index) {
+          if(line.indexOf(person) != (line.length - 1)) {
+            lineReport += (" " + (index + 1) + ". " + person + ",");
+          } else {
             lineReport += (" " + (index + 1) + ". " + person);
+          }
         });
         return lineReport;
     }
