8f029ca5c717b69b14a09fa45af21ac7a357dd6c
Seth Lachman <lachmanseth17@gmail.com>
2016-05-30 13:32:10 -0400

    Done.

--- deli.js
+++ deli.js
@@ -1,6 +1,28 @@
 var katzDeli = [];
 
-function takeANumber(line, new) {
-   katzDeli.push(new);
+function takeANumber(line, name) {
+   line.push(name);
+   return "Welcome, " + name + ". You are number " + line.length + " in line.";
+}
+
+
+function nowServing(line) {
+   if (line.length < 1) {
+    return "There is nobody waiting to be served!";
+  } else {
+  return  "Currently serving " + line.shift() + ".";
+  }
+}
+
+
+function currentLine(line) {
+  if (line.length < 1) {
+   return "The line is currently empty.";
+ } else
+    line.forEach(function(person, index) { var number = index + 1;
+      katzDeli.push(number + ". " + person);});
+    {
+    return "The line is currently: " + katzDeli.join(", ");
+ }
 
 }
