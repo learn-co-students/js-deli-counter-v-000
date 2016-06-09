5d28cb24ebcbf71596ffffec39a0895d913e17b1
Aleta Lanier <aleta.marie3@gmail.com>
2016-06-09 16:25:45 +0000

    Done.

---
+++ deli.js
@@ -0,0 +1,26 @@
+katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return "Welcome, " + name + "." + " You are number " + katzDeliLine.length + " in line.";
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length <= 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return "Currently serving " + katzDeliLine.shift() + "."
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length <= 0){
+    return "The line is currently empty.";
+  } else {
+    var phrase = "The line is currently: "
+    katzDeliLine.forEach(function(name, index) {
+       phrase += (index + 1) + '. ' + name + ', ';
+    });
+    return phrase.slice(0,-2)
+  }
+}
