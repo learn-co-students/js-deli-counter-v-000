5e38c687457f3649c1d105a84d9f05467acbfde4
agdavid <antonio.g.david@gmail.com>
2016-06-20 15:01:07 -0400

    refactor sample functions; commented out

--- deli.js
+++ deli.js
@@ -5,6 +5,11 @@
 //accepts current line (katzDeliLine) as parameter
 //returns position in line
 
+// function takeANumber(line, name) {
+//   line.push(name);
+//   return `Welcome, ${name}. You are number ${line.length} in line.`;
+// };
+
 function takeANumber(line, name) {
   line.push(name);
   var lineNumber = line.length;
@@ -15,6 +20,16 @@ function takeANumber(line, name) {
 //return the next person in line
 //remove the next person in line
 //if empty, return "There is nobody waiting to be served!"
+
+// function nowServing(line) {
+//   if (line.length === 0) {
+//     return "There is nobody waiting to be served!";
+//   } else {
+//     var currentCustomer = line[0];
+//     line.splice(0,1);
+//     return `Currently serving ${currentCustomer}.`
+//   }
+// };
 
 function nowServing(line) {
   if(line.length > 0){
@@ -30,7 +45,20 @@ function nowServing(line) {
 //currentLine
 //if people in line, returns the line
 //if empty, returns "The line is currently empty."
-//return current line 
+//return current line
+
+// function currentLine(line) {
+//   if (line.length === 0) {
+//     return "The line is currently empty."
+//   } else {
+//     var customers = [];
+//     line.forEach(function(customer, index) {
+//       customers.push(`${index+1}. ${customer}`);
+//       });
+//     return "The line is currently: " + customers.join(", ");
+//   }
+// };
+ 
 function currentLine(line) {
   if(line.length > 0) {
     var customerOrder = [];
