081122afa6b15ad2b77b2e98d3d677fe5f06b96d
Drew Pham <drewvpham@gmail.com>
2016-06-30 22:59:58 +0000

    Done.

--- deli.js
+++ deli.js
@@ -18,14 +18,15 @@ return "Welcome, "+name+". You are number "+ katzDeliLine.length+" in line.";
 
 
 function currentLine(line){
-if (line.length===0){
-  return "The line is currently empty.";}
-else {
-  var queue=[]
-  line.forEach(function(name, index){
- queue.push((index+1)+". "+name);
+if(line.length===0){
+  return "The line is currently empty.";
+}else {
+var queue=[]
 
-  });
+line.forEach(function(person, index){
+queue.push((index+1)+". "+person)
+});
   return "The line is currently: "+queue.join(", ");
 }
-};
+
+}
