b06b2516a65dcdd3629149d8b994d314d820e376
Jason Casares <jasonc@csdrafting.com>
2016-07-26 22:30:41 +0000

    Done.

--- deli.js
+++ deli.js
@@ -1,16 +1,27 @@
-var katzDeli = [];
-
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.forEach(function(name, index)) {
-    var number = index + 1;
-    return "Welcome " + name + " you are number " + number + " in line."
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
   }
-}
 
-function nowServing(name) {
+  const numbersAndNames = []
 
-}
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
 
-function currentLine(katzDeliLine) {
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
 
-}
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
