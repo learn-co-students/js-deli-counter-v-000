4ba1bf96ee6ae1df2c384f11c8613c84eed762fc
Jason Casares <jasonc@csdrafting.com>
2016-07-26 22:26:04 +0000

    Done.

---
+++ deli.js
@@ -0,0 +1,16 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.forEach(function(name, index)) {
+    var number = index + 1;
+    return "Welcome " + name + " you are number " + number + " in line."
+  }
+}
+
+function nowServing(name) {
+
+}
+
+function currentLine(katzDeliLine) {
+
+}
