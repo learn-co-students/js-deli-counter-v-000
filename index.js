a1bae3b491541edf0a03d6b021cbc40440c4c514
glee38 <glee3388@gmail.com>
2016-07-28 22:45:43 -0400

    refactor

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
+// function takeANumber(katzDeliLine, newPerson) {
+//   katzDeliLine.push(newPerson);
+//   var number = katzDeliLine.indexOf(newPerson) + 1;
+//   return `Welcome, ${newPerson}. You are number ${number} in line.`;
+// }
+
+// refactored:
+
 function takeANumber(katzDeliLine, newPerson) {
   katzDeliLine.push(newPerson);
-  var number = katzDeliLine.indexOf(newPerson) + 1;
-  return `Welcome, ${newPerson}. You are number ${number} in line.`;
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
 }
 
+// function nowServing(katzDeliLine) {
+//   if (katzDeliLine.length == 0) {
+//     return "There is nobody waiting to be served!";
+//   }
+//   else {
+//     var currentPerson = katzDeliLine.splice(0,1);
+//     return `Currently serving ${currentPerson}.`;
+//   }
+// }
+
+// refactored:
+
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length == 0) {
+  if (!katzDeliLine.length) {
     return "There is nobody waiting to be served!";
   }
   else {
-    var currentPerson = katzDeliLine.splice(0,1);
-    return `Currently serving ${currentPerson}.`;
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine.length == 0) {
+  if (!katzDeliLine.length) {
     return "The line is currently empty.";
   }
   else { 
