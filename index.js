c63ce1d008f4e1f5d170a6be9e8ce3213b6c56cf
ndaniele <n.daniele@gmail.com>
2016-09-02 10:29:39 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+  }
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {    
+    return `Currently serving ${katzDeliLine.splice(0,1)}.`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    currentLine = [];
+    katzDeliLine.forEach(function(person, index) {
+      currentLine.push(`${index+1}. ${person}`);
+    });
+    return `The line is currently: ${currentLine.join(", ")}`;      
+  } else {
+    return "The line is currently empty."
+  }
+}
