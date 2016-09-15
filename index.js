f99c2a9bb1d16fa51dbc01903c066e3769d2a0de
sensei100 <jenanton01@gmail.com>
2016-09-15 09:58:26 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+ function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+    return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+  }
+
+  function nowServing(katzDeliLine, name) {
+    if (!katzDeliLine.length) {
+      return "There is nobody waiting to be served!";
+    } else {
+      return "Currently serving " + katzDeliLine.splice(0, 1) + ".";
+    }
+  }
+
+  function currentLine(katzDeliLine) {
+      if (!katzDeliLine.length) {
+        return "The line is currently empty."
+      } else {
+        var currentLine = [];
+        katzDeliLine.forEach(function(name, index) {
+          currentLine.push((index + 1) + ". " + name)
+      });
+        return "The line is currently: " + currentLine.join(", ");
+    }
+  }
