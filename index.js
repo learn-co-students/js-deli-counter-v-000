1e5ab9765ad30238aa991c79f457438eb6c99959
lizdenhup <lizdenhup@gmail.com>
2016-12-09 21:55:31 -0600

    #takeANumber working

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var katzDeliLine = ["a", "b", "c", "d"];
+var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
-
+  katzDeliLine.push(`${name}`);
+  var place = katzDeliLine.length; 
+  return `Welcome, ${name}. You are number ${place} in line.`;
 }
 
 function nowServing(katzDeliLine) {
