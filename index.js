060eba2584db177824b08b1512a05567de64355c
bmolina-nyc <bruce.molina.81@gmail.com>
2017-01-14 17:43:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-var katzDeli = [];
-
-
-function takeANumber(katzDeli, name){
-  katzDeli.push(name)
-  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+function takeANumber(line, name){
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
 
-function nowServing(katzDeli){
-  if (katzDeli.length === 0) {
+function nowServing(line){
+  if (line.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    var nextUp = katzDeli.splice(0,1);
+    var nextUp = line.splice(0,1);
     return `Currently serving ${nextUp}.`;
   }
 }
