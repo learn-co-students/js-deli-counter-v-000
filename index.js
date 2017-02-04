9913ef40d9dec10f22ae68bb4b5e4d12a1d6aa1a
Emmanuel Owusu <e_owusu@live.com>
2017-02-04 23:03:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
+var katzDeliLine = [];
 
-var katzDeli = [];
 
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name); // add person to line
-  var position = katzDeliLine.length; // person's position is size of deli's length
-  return `Welcome, ${name}. You are number ${position} in line.`;
+function takeANumber(katzDeliLine, person){
+  katzDeliLine.push(person);
+  var position = katzDeliLine.length;
+  return `Welcome, ${person}. You are number ${position} in line.`;
 }
 
+
 function nowServing(katzDeliLine){
-  if(katzDeliLine.length == 0){
+  if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
   }
-
-    return `Currently serving ${katzDeliLine.shift()}.`;
+  return `Currently serving ${katzDeliLine.shift()}.`;
 }
 
 function currentLine(katzDeliLine){
-  if(katzDeliLine.length == 0){
+  if(katzDeliLine.length === 0){
     return "The line is currently empty.";
   }
-  const newArray = []
+
+  const newLine = [];
   for(let i=0, l=katzDeliLine.length; i<l; i++){
-    newArray.push(`${i+1}. ${katzDeliLine[i]}`)
+    newLine.push(`${i+1}. ${katzDeliLine[i]}`)
   }
-  return `The line is currently: ${newArray.join(', ')}`;
+  return `The line is currently: ${newLine.join(', ')}`;
 }
