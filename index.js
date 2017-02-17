7acdef027d0d139e0e43da3cc62e8c1960f67c4d
Benjamin Cantlupe <bjcantlupe@gmail.com>
2017-02-17 18:36:05 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.push(name)} in line.`;
-}
+};
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length < 1) {
@@ -10,20 +10,17 @@ function nowServing(katzDeliLine){
   } else {
     return `Currently serving ${katzDeliLine.shift()}.`;
   }
-}
+};
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length < 1) {
     return "The line is currently empty."
   } else {
-    var str = `The line is currently: `;
-    var strArr;
-    for(let i = 0; i < katzDeliLine.length; i++) {
-      str += `${i + 1}. ${katzDeliLine[i]}, `;
+    const numbersAndNames = [];
+
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      numbersAndNames.push(`${i + 1}. ${katzDeliLine[i]}`);
     }
-    strArr = str.trim().split('');
-    strArr.pop();
-    str = strArr.join("");
-    return str;
+    return `The line is currently: ${numbersAndNames.join(', ')}`
   }
-}
+};
