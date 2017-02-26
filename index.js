e1dbb9a3190232e2392c55c597c16c5590be5151
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-02-26 01:29:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+function takeANumber(katzDeliLine, name) {
+    katzDeliLine.push(name)
+    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  var front = katzDeliLine[0]
+  katzDeliLine.shift()
+  return `Currently serving ${front}.`
+}
+
+function currentLine(line){
+  if (line.length ===0){
+    return "The line is currently empty."
+  }
+  var lineArray =[]
+  for (let i = 0; i < line.length; i++){
+   lineArray.push(`${i + 1}. ${line[i]}`)
+  }
+ return `The line is currently: ${lineArray.join(', ')}`
+}
