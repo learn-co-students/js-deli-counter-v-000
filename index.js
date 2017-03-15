204ac741a93d360b1ade91b5d60339a14553e481
mcclains2003 <mcclains2003@gmail.com>
2017-03-15 10:54:42 -0500

    Done.

--- index.js
+++ index.js
@@ -15,19 +15,6 @@ function nowServing(array) {
   }
 }
 
-// function whileLoop(array) {
-//   var start = `The line is currently:`
-//   var i = 0  
-
-//   while (i < array.length) {
-//     var inLine = `${i + 1}. ${array[i]}`
-//     var newLine = `${start] ${test}`
-
-//     i++
-//   }
-//   return start
-// }
-
 function currentLine(array) {
   var i = 0
 
@@ -45,21 +32,3 @@ function currentLine(array) {
 
   return `The line is currently: ${line.join(', ')}`
 }
-
-
-
-
-// function currentLine(array) {
-//   if (!line.length) {
-//     return "The line is currently empty."
-//   }
-
-//   const numbersAndNames = []
-
-//   for (let i = 0, l = line.length; i < l; i++) {
-//     numbersAndNames.push(`${i + 1}. ${line[i]}`)
-//   }
-
-//   return `The line is currently: ${numbersAndNames.join(', ')}`
-// };
-
