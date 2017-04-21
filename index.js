4fa518f5c57aed8edaa8e5591fb21e252ab4df2c
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-04-21 05:24:51 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(array, name) {
+  array.push(name)
+
+  return `Welcome, ${name}. You are number ${array.length} in line.`
+}
+
+function nowServing(line) {
+  if (!!line[0] === true ) {
+    return `Currently serving ${line.shift()}.`
+  }
+  else {
+    return 'There is nobody waiting to be served!'
+  }
+}
+
+function currentLine(line) {
+  if (!!line[0] === true) {
+    var current_line = "The line is currently: "
+    for (var i = 0 ; i < line.length -1 ; i++) {
+      current_line += `${i + 1}. ${line[i]}, `
+    }
+    current_line += `${line.length}. ${line.slice(-1)}`
+    return current_line
+  } else {
+    return "The line is currently empty."
+  }
+}
