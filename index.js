2b6431ee89c656bdb10d8512906675c2945ffa53
Alex Glover <Alex@MacBook-Pro-3.local>
2017-04-25 11:38:50 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  var spot = katzDeliLine.length + 1
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${spot} in line.`
+};
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return `Currently serving ${line.shift()}.`
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    var list = []
+    for (let i = 0; i < line.length; i++) {
+      list.push(`${i + 1}. ${line[i]}`)
+    }
+    return `The line is currently: ${list.join(", ")}`
+  }
+}
