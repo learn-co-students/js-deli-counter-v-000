45c3a890392f47ac677c9f2225a233af4fd4befd
Leona Hu Hudelson <hqleona@gmail.com>
2017-05-26 23:40:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+var alreadyInline =["Steven", "Blake", "Avi"]
+
+function takeANumber(katzDeli, name) {
+  var katzDeliLine = []
+  names = ["Ada", "Grace", "Kent"]
+
+  for ( let i =0; i<names.length; i++) {
+    if(names[i] == name) {
+        return `Welcome ${name[i]}, You are ${i} in line`;
+    }
+  }
