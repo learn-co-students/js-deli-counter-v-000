e21c4bcc8b291727d52f55e5c5fc4a50185564c6
Bruce Molina <bruce.molina.81@gmail.com>
2017-07-25 04:30:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-function takeANumber(line, name){
-  line.push(name)
-  return `Welcome, ${name}. You are number ${line.length} in line.`;
+var katzDeli = [];
+
+function takeANumber(katzDeli, person){
+  katzDeli.push(person)
+  return `Welcome, ${person}. You are number ${katzDeli.length} in line.`
 }
 
-
-function nowServing(line){
-  if (line.length === 0) {
-    return "There is nobody waiting to be served!";
+function nowServing(katzDeli){
+  if (katzDeli.length != 0){
+    return `Currently serving ${katzDeli.shift()}.`
   } else {
-    return `Currently serving ${line.shift()}.`;
+    return "There is nobody waiting to be served!"
   }
 }
 
 function currentLine(line){
-  var numberedNames = []
-  
-  if (line.length === 0) {
-    return "The line is currently empty.";
-  } else {
-    line.forEach(function(name, index){
-      numberedNames.push(`${index+1}. ${name}`)
-    }) 
+  if (line.length === 0){
+    return 'The line is currently empty.'
   }
-  return `The line is currently: ${numberedNames.join(", ")}`
-}
+
+  var result = []
+
+  for (let i = 0 ; i < line.length; i ++){
+      result.push( `${i+1}. ${line[i]}`)
+    }
+    return `The line is currently: ${result.join(", ")}`
+  }
