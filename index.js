5667fc3f88b1d335cd9eeb89fc4b06977d809658
Chad Montoya <chadmontoya21@yahoo.com>
2017-08-12 17:40:41 +0000

    Done.

--- index.js
+++ index.js
@@ -15,15 +15,19 @@ function nowServing(katzDeliLine) {
   }
 }
 
-var katzDeliLine = {}
+
 function currentLine(line) {
   if(line.length <= 0) {
     return "The line is currently empty."
   }
   var firstString = "The line is currently:"
-  for(var i = 0; i<line.length-1; i++) {
-    firstString = firstString  +  ` ${i+1}. ${line[i]},`
+  for(var i = 0; i<line.length; i++) {
+    if(i === line.length-1 ) {
+    firstString = firstString + ` ${i+1}. ${line[i]}`
+    return firstString
+    } else {
+      firstString = firstString +  ` ${i+1}. ${line[i]},`
+    }
 }
-firstString = firstString + ` ${line.length}. Ann`
- return firstString
+return firstString
 }
