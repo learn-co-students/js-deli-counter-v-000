f4668d9497d566fceebe63ce7448c215b2874731
Christine Cua <cloudycua@gmail.com>
2019-01-30 18:03:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-function takeANumber(katzDeli, name){
-  katzDeli.push(name);
-  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+function takeANumber(line, name){
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
-function nowServing(katzDeliLine){
-  if (katzDeliLine[0] == undefined){
+function nowServing(line){
+  if (line[0] == undefined){
     return "There is nobody waiting to be served!";
   } else {
-    return katzDeliLine[0];
-    katzDeliLine.splice(0,1);
+    return `Currently serving ${line.shift()}.`;
   }
 }
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  } else {
+      const numbersAndNames = []
+
+      for (let i = 0, l = line.length; i < l; i++) {
+        numbersAndNames.push(`${i + 1}. ${line[i]}`)
+      }
+
+      return `The line is currently: ${numbersAndNames.join(', ')}`
+    }
+};
